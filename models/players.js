ee427f47ff83152157c406c582b9507fe4c84966
ranulfjn <ranulfnoronha@gmail.com>
2021-03-14 23:30:29 -0400

    models

--- models/players.js
+++ models/players.js
@@ -1,1 +1,22 @@
-import mongoose from 'mongoose'
+const mongoose =require ('mongoose');
+
+
+const PlayersSchema=mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true
+    
+    },
+    lastName:{
+        type:String,
+        required:true
+    
+    },
+    club:{
+        type:String,
+        default:'Manchester United'
+    
+    }
+})
+
+module.exports=mongoose.model('Players',PlayersSchema);
--- routes/players.js
+++ routes/players.js
@@ -1,9 +1,27 @@
 const  express = require ("express");
-
 const router= express.Router();
+const Players = require('../models/Players')
 
 router.get('/',(req,res)=>{
-    res.send('ALl players')
+    res.send('All players')
+})
+
+
+router.post('/', async (req,res)=>{
+    const players = new Players({
+        firstName:req.body.firstName,
+        lastName:req.body.lastName
+    })
+
+    try{
+        const newPlayer = await players.save()
+        res.send(newPlayer)
+        
+    } catch(err)
+    {
+        res.send("Message:"+err);
+    }
+  
 })
 
 module.exports=router;
