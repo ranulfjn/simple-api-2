d780217bbebc4435bfcbda23eff96dc322d869d6
ranulfjn <ranulfnoronha@gmail.com>
2021-03-15 20:58:16 -0400

    final

--- controllers/players.js
+++ controllers/players.js
@@ -1,3 +1,30 @@
+const Players = require('../models/Players')
+
+const getUser =async (req,res)=>{
+
+    try{
+        const players = await Players.find();
+        res.send(players)
+    } catch (err)
+    {
+        res.send("message:"+err);
+    }
+   
+}
+
+
+const getUserById =async(req,res)=>{
+    
+
+    try{
+        const foundPlayer = await Players.findById(req.params.id);
+        res.send(foundPlayer);
+    } catch(err){
+        res.send('message:'+ err);
+    }
+  
+}
+
 const addUser = async (req,res)=>{
     const players = new Players({
         firstName:req.body.firstName,
@@ -13,8 +40,38 @@ const addUser = async (req,res)=>{
         res.send("Message:"+err);
     }
   
+};
+const deletePlayer = async (req,res)=>{
+
+    try{
+        const removedPlayer = await Players.remove({ _id:req.params.id })
+        res.send(removedPlayer);
+    }catch(err){
+        res.send('Message:'+err)
+    }
+   
+
 }
+const updatePlayer = async (req,res)=>{
+    try{
+        const Updatedplayer = await Players.updateOne(
+            { _id:req.params.id } ,
+            { $set: { firstName:req.body.firstName} })
+        res.send(Updatedplayer);
+    }catch (err){
+        res.send("message:"+err)
+    }
+
+    
+
+};
 
 
 
-module.exports=addUser;
+module.exports={
+    addUser,
+    getUser,
+    getUserById,
+    deletePlayer,
+    updatePlayer
+};
--- routes/players.js
+++ routes/players.js
@@ -1,14 +1,14 @@
 const  express = require ("express");
 const router= express.Router();
-const Players = require('../models/Players')
-const addUser = require('../controllers/players')
-
-router.get('/',(req,res)=>{
-    res.send('All players')
-})
+const {addUser ,getUser ,getUserById ,updatePlayer ,deletePlayer }= require('../controllers/players')
 
 
+router.get('/',getUser)
 router.post('/', addUser)
+router.get('/:id',getUserById )
+router.patch('/:id',updatePlayer)
+router.delete('/:id',deletePlayer)
+
 
 module.exports=router;
 
